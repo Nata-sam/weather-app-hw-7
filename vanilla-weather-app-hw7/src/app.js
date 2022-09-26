b356edef6314dc9394f999470c51a73094096de1
Nata-sam <nataliasamsonova5@gmail.com>
2022-09-26 08:52:41 -0700

    js temperature

--- vanilla-weather-app-hw7/src/app.js
+++ vanilla-weather-app-hw7/src/app.js
@@ -1,1 +1,20 @@
-alert("hello");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let precipitationElement = document.querySelector("#precipitation");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  precipitationElement.innerHTML = response.data.main.precipitation;
+}
+
+let apiKey = "eba55a69377166cdfc966e8ada8ca883";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
